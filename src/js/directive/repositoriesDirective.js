a34fb327e750d7a297df7dce275026dfe693526b
cwreden <christian.wreden@googlemail.com>
2013-12-24 12:31:53 +0100

    Bootstrap integrated.

--- src/js/directive/repositoriesDirective.js
+++ src/js/directive/repositoriesDirective.js
@@ -8,13 +8,17 @@ angular.module('github-directives')
                 'ghUser': '@'
             },
             template:
-                '<span>{{ ghUser }}</span>' +
-                    '<ul>' +
-                        '<li ng-repeat="repo in repos">' +
-                            '<a href="{{ repo.html_url }}" target="_blank">{{ repo.name }}</a> ' +
-                            '<span style="font-size: 11px;">(Open Issues: {{ repo.open_issues }} - Forks: {{ repo.forks }})</span>' +
-                        '</li>' +
-                    '</ul>',
+                '<div class="panel panel-default">' +
+                    '<div class="panel-heading">Repositories: {{ ghUser }}</div>' +
+                    '<div class="panel-body">' +
+                        '<ul>' +
+                            '<li ng-repeat="repo in repos">' +
+                                '<a href="{{ repo.html_url }}" target="_blank">{{ repo.name }}</a> ' +
+                                '<span style="font-size: 11px;">(Open Issues: {{ repo.open_issues }} - Forks: {{ repo.forks }})</span>' +
+                            '</li>' +
+                        '</ul>' +
+                    '</div>' +
+                '</div>',
             controller: ['$scope', '$http', 'apiUrl', function($scope, $http, apiUrl) {
                 $scope.repos = [];
 
--- src/js/directive/repositoryCommits.js
+++ src/js/directive/repositoryCommits.js
@@ -9,13 +9,17 @@ angular.module('github-directives')
                 'ghRepo': '@'
             },
             template:
-                '<span>{{ ghUser }} - {{ ghRepo }}</span>' +
-                    '<ul>' +
-                    '<li ng-repeat="commit in commits">' +
-                    '<a href="{{ commit.html_url }}" target="_blank">{{ commit.commit.message }}</a> ' +
-                    '<span style="font-size: 11px;">By: {{ commit.commit.author.name }}</span>' +
-                    '</li>' +
-                    '</ul>',
+                '<div class="panel panel-default">' +
+                    '<div class="panel-heading">Commits: {{ ghUser }} - {{ ghRepo }}</div>' +
+                    '<div class="panel-body">' +
+                        '<ul>' +
+                            '<li ng-repeat="commit in commits">' +
+                                '<a href="{{ commit.html_url }}" target="_blank">{{ commit.commit.message }}</a> ' +
+                                '<span style="font-size: 11px;">By: {{ commit.commit.author.name }}</span>' +
+                            '</li>' +
+                        '</ul>' +
+                    '</div>' +
+                '</div>',
             controller: ['$scope', '$http', 'apiUrl', function($scope, $http, apiUrl) {
                 $scope.commits = [];
 
