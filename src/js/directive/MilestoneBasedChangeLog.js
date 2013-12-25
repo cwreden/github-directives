25ef68368f8211f1301b3f9065a6b54a9a3e8934
cwreden <christian.wreden@googlemail.com>
2013-12-25 11:02:52 +0100

    Milestone based changelog directive implemented.

---
+++ src/js/directive/MilestoneBasedChangeLog.js
@@ -0,0 +1,41 @@
+'use strict'
+
+angular.module('github-directives')
+    .directive('ghMilestoneChangeLog', function () {
+        return {
+            restrict: 'EA',
+            scope: {
+                'user': '@',
+                'repo': '@',
+                'milestone': '@'
+            },
+            templateUrl: 'src/template/MilestoneChangeLog.html',
+            controller: ['$scope', '$http', 'apiUrl', function($scope, $http, apiUrl) {
+                $scope.issues = [];
+
+                $scope.$watch('milestone', function () {
+                    if ($scope.repo == undefined || $scope.user == undefined || $scope.milestone == undefined) {
+                        return;
+                    }
+                    $http({
+                        method: 'GET',
+                        url: apiUrl + '/repos/' + $scope.user + '/' + $scope.repo + '/issues',
+                        params: {
+                            state: 'closed'
+                        }
+                    })
+                        .success(function (data) {
+                            $scope.issues = [];
+                            if ($scope.milestone !== undefined) {
+                                data.forEach(function (issue) {
+                                    if (issue.milestone !== null && issue.milestone.title == $scope.milestone) {
+                                        $scope.issues.push(issue);
+                                    }
+                                });
+                            }
+                        });
+                });
+            }]
+        };
+    });
+
