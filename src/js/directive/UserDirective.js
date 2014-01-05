424f00005a56e421769173898454dd98e8f3cadc
cwreden <christian.wreden@googlemail.com>
2014-01-06 00:54:46 +0100

    Visibility of all elements is now configurable

--- src/js/directive/UserDirective.js
+++ src/js/directive/UserDirective.js
@@ -1,30 +1,39 @@
 'use strict'
-
+var bla;
 angular.module('github-directives')
     .directive('ghUser', function () {
         return {
             restrict: 'E',
             scope: {
                 'name': '@',
-                'showAvatar': '@',
-                'showBlog': '@'
+                'showAvatar': '=',
+                'avatarSize': '@',
+                'showBlog': '=',
+                'showCompany': '=',
+                'showGists': '=',
+                'hideRepositories': '=',
+                'hideFollowers': '=',
+                'hideFollowing': '='
             },
             template:
                 '<div class="panel panel-default">' +
                     '<div class="panel-heading">{{ user.name }}</div>' +
                     '<div class="panel-body">' +
                         '<div ng-show="showAvatar">' +
-                            '<img src="{{ user.avatar_url }}">' +
+                            '<a target="_blank" href="{{ user.html_url }}"><img src="{{ user.avatar_url }}" style="width: {{ avatarSize }}; height: {{ avatarSize }};"></a>' +
                             '</div>' +
-                            '<div>Repositories: <a target="_blank" href="https://github.com/{{ name }}?tab=repositories">{{ user.public_repos }}</a></div>' +
-                            '<div>Gists: {{ user.public_gists }}</div>' +
-                            '<div>Followers: <a target="_blank" href="https://github.com/{{ name }}/followers">{{ user.followers}}</a></div>' +
-                            '<div>Following: <a target="_blank" href="https://github.com/{{ name }}/following">{{ user.following}}</a></div>' +
+                            '<div ng-show="showCompany">Company: {{ user.company }}</div>' +
+                            '<div ng-hide="hideRepositories">Repositories: <a target="_blank" href="https://github.com/{{ name }}?tab=repositories">{{ user.public_repos }}</a></div>' +
+                            '<div ng-show="showGists">Gists: {{ user.public_gists }}</div>' +
+                            '<div ng-hide="hideFollowers">Followers: <a target="_blank" href="https://github.com/{{ name }}/followers">{{ user.followers}}</a></div>' +
+                            '<div ng-hide="hideFollowing">Following: <a target="_blank" href="https://github.com/{{ name }}/following">{{ user.following}}</a></div>' +
                             '<a ng-show="showBlog && user.blog" href="{{ user.blog }}" target="_blank">Blog</a>' +
                         '</div>' +
                     '</div>',
             controller: ['$scope', '$http', 'apiUrl', function($scope, $http, apiUrl) {
                 $scope.user = {};
+
+                $scope.avatarSize = $scope.avatarSize || '100px';
 
                 $scope.$watch('name', function () {
                     if ($scope.name == undefined) {
@@ -32,6 +41,7 @@ angular.module('github-directives')
                     }
                     $http({method: 'GET', url: apiUrl + '/users/' + $scope.name})
                         .success(function (data) {
+                            console.log($scope)
                             $scope.user = data;
                         })
                         .error(function (data, status) {
